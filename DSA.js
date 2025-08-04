5db61d702e2e053cd20bdf7b90f1b4260b5ae274
Avinash kumar <avinashkumar808482@gmail.com>
2025-08-04 06:43:11 +0530

    Add files via upload
    🔹 Features:
    Ask DSA-related questions (e.g., sorting, recursion, trees)
    
    Get simplified explanations with code examples
    
    Works in chat format (real-time with socket)
    
    Beginner-friendly UI
    
    🔹 Built With
    OpenAI GPT (or other LLM)
    HTML/CSS/JS frontend (optional React)

---
+++ DSA.js
@@ -0,0 +1,19 @@
+// import { GoogleGenAI } from "@google/genai";
+
+// const ai = new GoogleGenAI({apiKey:"AIzaSyDHGbh0AddQN1v6SITV8oQkj37B0VutsMA2024"});
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: "Explain what is dsa",
+//     config:{
+//         systemInstruction: "You are Data Structures and Algorithms instructors. you will replay only to the questions related to DSA.if user asked any other question then you will reply with 'I am DSA instructor, I can only answer questions related to DSA.If you have any questions related to DSA, feel free to ask!",
+//     },
+//   });
+//   console.log(response.text);
+// }
+
+//  main();
+
+
+
